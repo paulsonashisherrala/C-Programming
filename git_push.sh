fe1e2bbe8813f9d82fe20b6fb4b6c67e9e5d4ed1
paulsonashisherrala <ashishroy007@localhost.localdomain>
2024-12-08 09:56:42 +0000

    updated git_push.sh

--- git_push.sh
+++ git_push.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 
+# Set the GitHub username
+username="paulsonashisherrala"  # Hardcoded GitHub username
+
 # Ask for commit message from the user
 echo "Enter commit message:"
-read commit_message
+read commit_message 
 
+# Stage all changes for commit
 git add .
+
+# Pull the latest changes from the remote repository to avoid conflicts
 git pull
+
+# Commit the staged changes with the provided commit message
 git commit -m "$commit_message"
-echo "Enter your GitHub username:"
-read username
+
+#echo "Enter your GitHub username:"
+#read username
+
 
 # Ask for the GitHub Personal Access Token (PAT) instead of the password
 echo "Enter your GitHub Personal Access Token (PAT):"
 read -s token  # The '-s' option hides the input for security
 
-# Construct the remote URL using the username and PAT
-REMOTE_URL="https://$username:$token@github.com/paulsonashisherrala/C-Programming.git"
+# Construct the remote URL using the hardcoded username and PAT
+REMOTE_URL="https://$username:$token@github.com/$username/C-Programming.git"
 
 # Push the changes using the constructed remote URL
 git push $REMOTE_URL
@@ -31,6 +41,6 @@ if [ $? -eq 0 ]; then
     done
     echo -e "\n🎉✨ DONE! Your changes have been pushed successfully! 🚀🎉"
 else
-    echo "Error during push!"  # Error message in case of failure
+    # Error message in case of failure
+    echo "Error during push!"  
 fi
-
